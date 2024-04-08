d735e19863de0256cf5ca715f9235fd08c3de9a0
amanafro <amanauelisaak15@gmail.com>
2024-04-08 22:29:12 +0200

    debugging is such a pain smh

--- main.go
+++ main.go
@@ -7,39 +7,40 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type Account struct {
+	ID         int
+	Balance    int
+	CustomerID int
+}
+
 type Customer struct {
 	ID       int
 	Name     string
 	Password string
 }
 
-type Account struct {
-	ID         int
-	Balance    float64
-	CustomerID int
-}
-
-func checkErr(err error) {
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/gobank")
 	if err != nil {
 		panic(err)
 	}
-}
-
-func main() {
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/gobank")
-	checkErr(err)
 
 	defer db.Close()
 
 	result, err := db.Query("SELECT a.account_id, a.account_number, a.balance, c.name AS customer_name FROM   accounts a  JOIN customers c ON a.customer_id = c.customer_id;")
-	checkErr(err)
+	if err != nil {
+		panic(err)
+	}
 
 	for result.Next() {
-		var cusomer Customer
 		var account Account
+		var cusomer Customer
 
-		err = result.Scan(&cusomer.Name, &account.CustomerID, &account.Balance)
-		checkErr(err)
+		err = result.Scan(&cusomer.ID, &cusomer.Name, &account.CustomerID, &account.Balance)
+		if err != nil {
+			panic(err)
+		}
+
 		fmt.Printf("Customer: %s, CustomerID: %d, Balance: %f", cusomer.Name, account.CustomerID, account.Balance)
 	}
 }
